bd8c188f285906101d4ad815cd1f9faec0fdf722
Daniel Cruz <leinadrocks_2019_@hotmail.com>
2019-06-23 05:22:54 -0400

    metodo Put Cliente v1.0

--- routes/api/v1.0/services.js
+++ routes/api/v1.0/services.js
@@ -262,4 +262,43 @@ router.patch(/cliente\/[a-z0-9]{1,}$/, (req, res) => {
   });
 });
 
+//Actualiza los datos del cliente
+router.put(/cliente\/[a-z0-9]{1,}$/, (req, res) => {
+  var url = req.url;
+  var id = url.split("/")[2];
+  var keys  = Object.keys(req.body);
+  var oficialkeys = ['nombre', 'apellidos','ci', 'telefono', 'email',];
+  var result = _.difference(oficialkeys, keys);
+  if (result.length > 0) {
+    res.status(400).json({
+      "msn" : "erorr no se puede actualizar intenten con patch"
+    });fmulter
+    return;
+  }
+  var cliente = {
+    nombre : req.body.nombre,
+    apellidos : req.body.apellidos,
+    ci : req.body.ci,
+    telefono : req.body.telefono,
+    email : req.body.email,
+
+  };
+  Cliente.findOneAndUpdate({_id: id}, cliente, (err, params) => {
+      if(err) {
+        res.status(500).json({
+          "msn": "Error no se pudo actualizar los datos"
+        });
+        return;
+      }
+      res.status(200).json({
+        "resp": 200,
+        "dato": cliente,
+        "msn" :  "cliente  editado con exito"
+      });
+      return;
+  });
+});
+
+
+
 module.exports = router;
